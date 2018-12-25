363e3158991253d3bb4863f364c1f8fd7435d05b
SHAD RAYHAN MAZUMDER <shadmazumder@shads-macbook-pro.local>
2018-12-26 00:47:08 +0600

    Error definition through enum

--- Love for Enum, continuing.playground/Contents.swift
+++ Love for Enum, continuing.playground/Contents.swift
@@ -1,6 +1,62 @@
 import Foundation
 
 /*:
+ Error definition through enum
+ */
+enum Mismatched : String{
+    case notFound
+    case undefined
+    case invalid
+}
+
+extension Mismatched: Error{}
+
+func find(text: String?, on sentence: String?) throws {
+    guard let text = text, text.count > 0 else { throw Mismatched.undefined }
+    guard let sentence = sentence, sentence.count > 0 else { throw Mismatched.invalid }
+    
+    if !sentence.contains(text) { throw Mismatched.notFound }
+}
+
+do {
+    try find(text: nil, on: "Some input")
+} catch Mismatched.undefined { Mismatched.undefined.rawValue }
+
+do {
+    try find(text: "any", on: nil)
+} catch Mismatched.invalid { Mismatched.invalid.rawValue }
+
+do {
+    try find(text: "any", on: "Nothing is here")
+} catch Mismatched.notFound { Mismatched.notFound.rawValue }
+
+do {
+    try find(text: "any", on: "any")
+    "Gotcha"
+} catch Mismatched.notFound { Mismatched.notFound.rawValue }
+
+/*:
+ Enum mutation, code smelling
+ */
+enum State{
+    case notConnected
+    case connecting
+    case disconnected
+    case connected(String)
+    
+    indirect case currentState(State)
+}
+
+extension State{
+    mutating func updateCurrentState(state: State) {
+        self = .currentState(state)
+    }
+}
+
+var state = State.connecting
+state.updateCurrentState(state: .connected("Oh ya"))
+
+/*:
  Recursive enum
  */
 indirect enum Buffer{
